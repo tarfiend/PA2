a362b27327757d1606d14e645fe44f100bd4bd6b
Henrique <130559472+tarfiend@users.noreply.github.com>
2023-05-29 09:23:46 -0300

    E1.py

--- LISTA 1/E1.py
+++ LISTA 1/E1.py
@@ -3,7 +3,7 @@ lista = [5, 2, 4, 1, 3, 2, 4]
 def sum_to_i(lista):
     n = len(lista)
     for i in range(0, n-1):
-        lista[i+1] = lista[i] + lista[i+1]
+        lista[i+1] += lista[i]
 
     return lista
 
